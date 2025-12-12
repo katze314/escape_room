27f5b55dbbd0a2cb15f8f182b08b804359af62fd
katze314 <marsmaennchen@t-online.de>
2025-12-12 17:16:12 +0100

    riddle1-3 working

--- script3.js
+++ script3.js
@@ -1,25 +1,38 @@
 var all_guesses="";
 var last_guess="";
 var correct="1434";
-const next_hint=[0,0,0,0];
-const hint_given=[false,false,false,false];
+const classes=["false","semi","full"];
+
 function check(){
-    last_guess=document.getElementById("input").innerHTML;
+    const next_hint=[0,0,0,0];
+    const hint_given=[false,false,false,false];
+    last_guess=document.getElementById("input").value;
+    var all_true=true;
     for(let i=0; i<4; i++){
         if (last_guess[i]==correct[i]){
             hint_given[i]=true;
             next_hint[i]=2;
+        }else{
+            all_true=false;
         }
+    }
+    if(all_true==true){
+        window.location.href="./4.html";
     }
     for(let i=0; i<4; i++){
         if(hint_given[i]==false){
             for(let j=0; j<4; j++){
                 if (last_guess[j]==correct[i]&&next_hint[j]==0){
-                    next_hint[i]=1;
+                    next_hint[j]=1;
                     hint_given[i]=true;
                 }
             }
         }
     }
+    for(let i=0; i<4; i++){
+        all_guesses+="<span class="+classes[next_hint[i]]+">"+last_guess[i]+"</span>";
+    }
+    all_guesses+="<br>";
+    document.getElementById("guessed").innerHTML=all_guesses;
 
 }
